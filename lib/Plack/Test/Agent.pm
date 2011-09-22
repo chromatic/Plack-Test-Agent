8901e2cd33a0f83c0065a8992465cf87ed416aad
chromatic <chromatic@wgz.org>
2011-09-21 17:41:40 -0700

    Ported 2args.t from Plack::Test to P::T::Agent.

--- lib/Plack/Test/Agent.pm
+++ lib/Plack/Test/Agent.pm
@@ -1,6 +1,33 @@
-package Plack::Test::Agent
+package Plack::Test::Agent;
+# ABSTRACT: OO interface for testing low-level Plack/PSGI apps
 
 use strict;
 use warnings;
+
+use HTTP::Request;
+use HTTP::Response;
+use HTTP::Message::PSGI;
+
+use Plack::Util::Accessor qw( app );
+
+sub new
+{
+    my ($class, %args) = @_;
+
+    my $self = bless {}, $class;
+    $self->app( delete $args{app} );
+
+    return $self;
+}
+
+sub get
+{
+    my ($self, $uri) = @_;
+    my $req          = HTTP::Request->new( GET => $uri );
+    my $env          = $req->to_psgi;
+    my $app          = $self->app;
+
+    return HTTP::Response->from_psgi( $app->( $env ) );
+}
 
 1;
